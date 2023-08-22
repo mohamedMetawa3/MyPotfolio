a50c384b7cbe8f5eee92e952f84ae9bb23664716
Mohamed Metawa <142787759+mohamedMetawa3@users.noreply.github.com>
2023-08-22 08:47:33 +0300

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import Logo from "/assets/logo1.png";
+import { FaBars, FaLinkedin, FaTimes, FaGithub } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [navMenu, setNavMenu] = useState(false);
+  const toggleNavMenu = () => {
+    setNavMenu(!navMenu);
+  };
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="ImageLogo" style={{ width: "50px" }} />
+      </div>
+      {/* menu */}
+      <div>
+        <ul className="hidden md:flex ">
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/*Humberger icon menu */}
+      <div onClick={toggleNavMenu} className="md:hidden z-10">
+        {!navMenu ? <FaBars /> : <FaTimes />}
+      </div>
+      {/*mobil menu */}
+      <ul
+        className={
+          !navMenu
+            ? "hidden"
+            : "absolute justify-center flex flex-col items-center top-0 left-0 bg-inherit h-screen w-full"
+        }
+      >
+        <li className="text-4xl py-6 ">Home</li>
+        <li className="text-4xl py-6 ">About</li>
+        <li className="text-4xl py-6 ">Skills</li>
+        <li className="text-4xl py-6 ">Work</li>
+        <li className="text-4xl py-6 ">Contact</li>
+      </ul>
+      {/* Social icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/mohamed-metawa"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/mohamedMetawa3"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
